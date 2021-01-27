51af1dd5dfa8d19ac1e01228612ea9948d898063
besemuna <besemuna@gmail.com>
2021-01-27 13:35:55 +0000

    add lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Samuel Besemun Adjei on 27/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
