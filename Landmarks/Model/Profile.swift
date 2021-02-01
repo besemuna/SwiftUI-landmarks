64616dd2d8d89163763281d9b65b082fb5b3a592
besemuna <besemuna@gmail.com>
2021-02-01 13:50:32 +0000

    add ui controls

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,28 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Samuel Besemun Adjei on 01/02/2021.
+//
+
+import Foundation
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+    }
+}
